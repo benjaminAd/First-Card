2efa9952b4ca9ec3020857c71abd3b2c126faadb
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-09-28 16:20:17 +0200

    Essai responsive

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isSmallHeight = MediaQuery.of(context).size.height < 700 ;
+    print(isSmallHeight);
     return Scaffold(
         appBar: AppBar(
           title: Text("First Card"),
@@ -15,43 +17,43 @@ class HomePage extends StatelessWidget {
             alignment: Alignment.center,
             children: [
               Container(
-                width: MediaQuery.of(context).size.width / 1.2,
-                height: MediaQuery.of(context).size.height / 4,
+                width: isSmallHeight ? MediaQuery.of(context).size.width * 0.90 : MediaQuery.of(context).size.width * 0.90,
+                height: isSmallHeight ? MediaQuery.of(context).size.height * 0.25 : MediaQuery.of(context).size.height * 0.2,
                 decoration: BoxDecoration(
                     color: Colors.pink,
                     borderRadius: BorderRadius.circular(15.0)),
               ),
               Container(
-                margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.width/1.8),
+                margin: EdgeInsets.only(
+                    bottom: isSmallHeight ? MediaQuery.of(context).size.width * 0.45 : MediaQuery.of(context).size.width * 0.43),
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.blue,
                 ),
                 child: Image.asset(
                   "images/1000emote1.png",
-                  width: MediaQuery.of(context).size.width / 5,
+                  width: isSmallHeight ? MediaQuery.of(context).size.width * 0.25 : MediaQuery.of(context).size.width * 0.2,
                 ),
-
               ),
               Container(
-                child: Text("ADOLPHE Benjamin"),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: MediaQuery.of(context).size.width/9),
+                margin: EdgeInsets.only(bottom: isSmallHeight ? MediaQuery.of(context).size.width * 0.1 : MediaQuery.of(context).size.width * 0.1),
                 child: Text("benjamin.adolphe@etu.umontpellier.fr"),
               ),
               Container(
-                margin: EdgeInsets.only(top: MediaQuery.of(context).size.width/4.5 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.person,
-                    ),
-                    Text("benjaminadolphe.fr")
-                  ],
-                )
-              )
+                margin: EdgeInsets.only(top: isSmallHeight ? MediaQuery.of(context).size.width * 0.05 : MediaQuery.of(context).size.width * 0.05),
+                child: Text("ADOLPHE Benjamin"),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: isSmallHeight ? MediaQuery.of(context).size.width * 0.2 : MediaQuery.of(context).size.width * 0.2),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.person,
+                      ),
+                      Text("benjaminadolphe.fr")
+                    ],
+                  ))
             ],
           ),
         ));
