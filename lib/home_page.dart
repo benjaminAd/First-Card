303bba3c809f2622c1f8673fa19e2dae76490ce2
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-09-28 15:34:10 +0200

    1er aperçu app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,24 +10,49 @@ class HomePage extends StatelessWidget {
           title: Text("First Card"),
         ),
         body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
           alignment: Alignment.center,
-          child: Container(
-            width: MediaQuery.of(context).size.width / 1.5,
-            height: MediaQuery.of(context).size.height / 5,
-            decoration: BoxDecoration(
-                color: Colors.pink, borderRadius: BorderRadius.circular(15.0)),
-            child: Column(
-              children: [
-                Container(
-                  child: Image.asset(
-                      "images/1000emote1.png",
-                    width: MediaQuery.of(context).size.width/5,
-                  ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                height: MediaQuery.of(context).size.height / 4,
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(15.0)),
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.width/1.8),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
                 ),
-              ],
-            ),
+                child: Image.asset(
+                  "images/1000emote1.png",
+                  width: MediaQuery.of(context).size.width / 5,
+                ),
+
+              ),
+              Container(
+                child: Text("ADOLPHE Benjamin"),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.width/9),
+                child: Text("benjamin.adolphe@etu.umontpellier.fr"),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.width/4.5 ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.person,
+                    ),
+                    Text("benjaminadolphe.fr")
+                  ],
+                )
+              )
+            ],
           ),
         ));
   }
